39e48a3007cb1197e2e6571faca78fffa9acd33a
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-09-26 15:54:39 +0200

    comments

---
+++ HELP.md
@@ -0,0 +1,5 @@
+Navigate To Autowired Candidates gutter action doesn't work 
+for the components autowired in the different module beans.
+
+
+Check the repository and service classes in the `jpa-module`
--- app-module/src/main/java/com/mytests/spring/appmodule/AppModuleApplication.java
+++ app-module/src/main/java/com/mytests/spring/appmodule/AppModuleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 // same results in case of @Import and in case of scanning
+// beans in local extra scanned package are ok
 @SpringBootApplication(scanBasePackages = {"com.mytests.spring.appmodule", "com.mytests.spring.jpamodule", "com.mytests.spring.extrapackage"})
 //@Import(JpaModuleConfig.class)
 public class AppModuleApplication {
